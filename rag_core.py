1183e52e34db07afcaffae5c27e656d51510f62f
gangthadepv@gmail.com <gangthadepv@gmail.com>
2026-07-05 14:08:38 +0530

    changed vector db to FAISS

--- rag_core.py
+++ rag_core.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.vectorstores import InMemoryVectorStore
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
@@ -9,21 +9,35 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 load_dotenv()
 
 
-_llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
+def build_vector_db(pdf_path: str) -> FAISS:
 
-def build_vector_db(pdf_path: str) -> InMemoryVectorStore:
     loader = PyPDFLoader(pdf_path)
     docs = loader.load()
 
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
     chunks = splitter.split_documents(docs)
 
-    embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-2-preview")
-    return InMemoryVectorStore.from_documents(documents=chunks, embedding=embeddings)
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="gemini-embedding-2-preview"
+    )
+
+    vector_db = FAISS.from_documents(
+        documents=chunks,
+        embedding=embeddings
+    )
+
+    vector_db.save_local("faiss_index")
+
+    return vector_db
 
 
-def answer_query(vector_db: InMemoryVectorStore, query: str, k: int = 2) -> str:
+def answer_query(vector_db: FAISS, query: str, k: int = 2) -> str:
     documents = vector_db.similarity_search(query=query, k=k)
     context = ""
 
@@ -34,5 +48,5 @@ def answer_query(vector_db: InMemoryVectorStore, query: str, k: int = 2) -> str:
                  context:{context} and
                  question is : {query} """
 
-    result = _llm.invoke(prompt)
+    result = llm.invoke(prompt)
     return result.content
